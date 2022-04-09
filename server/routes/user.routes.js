4dd0cf962a9388f667ce547b56a504e093a28e8a
Narek <narek1985ch@gmail.com>
2022-04-09 20:19:32 +0200

    some fixed in routes

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 
+router.get("/user", async (req, res) => {
+   
+});
 
+router.patch("/user/:email", async (req, res) => {
+   
+});
 
 module.exports = router;
--- server/routes/userCity.routes.js
+++ server/routes/userCity.routes.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 
+router.post("/userCity", async (req, res) => {
+   
+});
+
+router.get("/userCity/:email", async (req, res) => {
+   
+});
 
 module.exports = router;
